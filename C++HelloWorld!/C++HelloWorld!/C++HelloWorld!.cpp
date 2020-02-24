2a839e7ad0800d37f555c04d198cc4ce545093b0
o0serena0o <57519051+o0serena0o@users.noreply.github.com>
2020-02-24 22:43:05 +0900

    cin>> cout<< endl(end line)==\n

---
+++ C++HelloWorld!/C++HelloWorld!/C++HelloWorld!.cpp
@@ -0,0 +1,23 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int a, b;
+
+int main()
+{
+	cout << "Hello,Wolrd!" << endl;
+	cin >> a >> b;
+	cout << a << "+" << b << "=" << a + b << endl;
+	string str;
+	str = "ByeWolrd!";
+	cout << str << endl;
+
+	string name;
+	cout << "이름입력:";
+	cin >> name;
+	string message = "안녕하세요," + name + "님";
+	cout << message << endl;
+}
+
+//endl=end line == 줄바꿈 == /n
